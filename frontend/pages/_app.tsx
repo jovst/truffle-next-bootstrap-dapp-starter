79de66a91b5113d71c6de8043f86d1be8aea1f57
jovst <->
2022-04-07 16:05:16 +0200

    added basic SASS structure

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import '../styles/main.css'
+import '../styles/main.scss'
 
 
 import type { AppProps } from 'next/app'
